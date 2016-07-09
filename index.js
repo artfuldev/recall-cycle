e43e99a1b4c3e514da5a63f0c31719d8cb4ca26c
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-07-10 03:31:25 +0530

    Attempting reducers

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ import xs from 'xstream';;
 import delay from 'xstream/extra/delay';
 import { run } from '@cycle/xstream-run';
 import { div, h1, ul, li, a, p, span, makeDOMDriver } from '@cycle/dom';
+import Immutable from 'immutable';
 
 function intent({ dom }) {
+
   const newGame$ = dom
     .select('.new')
     .events('click')
@@ -13,6 +15,7 @@ function intent({ dom }) {
       return true;
     })
     .startWith(true);
+
   const reset$ = dom
     .select('.reset')
     .events('click')
@@ -20,25 +23,26 @@ function intent({ dom }) {
       ev.preventDefault();
       return true;
     });
-  const cellClicks$ = dom
+
+  const selectCell$ = dom
     .select('.cell')
     .events('click')
     .map(ev => parseInt(
       (ev.target.tagName == 'SPAN'
         ? ev.target.parentElement
         : ev.target).attributes['data-index'].value));
+
   return {
     newGame$,
     reset$,
-    cellClicks$
+    selectCell$,
+    any$: xs.merge(newGame$, reset$, selectCell$)
   };
 }
 
-function model(intent) {
-  const grid = [];
-  for (var i = 0; i < 25; i++)
-    grid.push(i);
-  const puzzle$ = intent.newGame$.map(() => {
+function reducers(actions) {
+
+  const newPuzzleReducer$ = actions.newGame$.map(state => {
     const puzzle = [];
     const maxSize = 9;
     for (var i = 0; i < maxSize; i++) {
@@ -47,33 +51,61 @@ function model(intent) {
         nextNumber = Math.floor(Math.random() * 25);
       puzzle.push(nextNumber);
     }
-    return puzzle;
+    return state.set('puzzle', puzzle);
   });
-  const userInputAllowed$ = xs.merge(intent.newGame$.mapTo(false), intent.newGame$.compose(delay(4000)).mapTo(true));
-  const userSelectedCells$ = xs.merge(userInputAllowed$.map(allowed => intent.cellClicks$.filter(() => allowed)).flatten()
-    .fold((selectedCells, clicked) => {
-      selectedCells = selectedCells || [];
-      var index = selectedCells.indexOf(clicked);
-      if (index === -1)
-        selectedCells.push(clicked);
-      else
-        selectedCells.splice(index, 1);
-      return selectedCells;
-    }, []), intent.reset$.mapTo([]));
-  const state$ = xs.combine(puzzle$, userInputAllowed$, userSelectedCells$).map(a => {
-    return {
-      puzzle: a[0],
-      userInputAllowed: a[1],
-      userSelectedCells: a[2],
-      grid
-    };
-  });
+
+  const allowedReducer$ = xs.merge(
+    actions.newGame$.map(state => state.set('allowed', false)),
+    actions.newGame$.compose(delay(4000)).map(state => state.set('allowed', true))
+  );
+
+  const selectedReducer$ = xs.merge(actions.selectCell$.map(state => {
+    if (!state.get('allowed'))
+      return state;
+    state.selected.set(state.selected || Immutable.List());
+    var index = state.selected.indexOf(clicked);
+    if (index === -1)
+      return state.set('selected', state.selected.push(clicked));
+    else
+      return state.set('selected', selected.filter(x => x != clicked));
+  }),
+    actions.reset$.map(state => state.set('selected', []))
+  );
+
+  return xs.merge(
+    newPuzzleReducer$,
+    allowedReducer$,
+    selectedReducer$
+  );
+}
+
+function model(actions) {
+  var grid = Immutable.List();
+  for (var i = 0; i < 25; i++)
+    grid = grid.push(i);
+  const reducer$ = reducers(actions);
+  const state$ = actions.any$
+    .mapTo(Immutable.Map(
+      {
+        grid,
+        puzzle: Immutable.List(),
+        allowed: false,
+        selectedCells: Immutable.List()
+      }
+    ))
+    .map(state => reducer$.fold((acc, reducer) => reducer(acc), state))
+    .flatten();
   return state$;
 }
 
 function view(state$) {
   const vtree$ = state$.map(state =>
-    div('#root', [
+  {
+    const grid = state.get('grid');
+    const allowed = state.get('allowed');
+    const puzzle = state.get('puzzle');
+    const selected = state.get('selected');
+    return div('#root', [
       div('.container', [
         div('.title.bar', [
           h1(['Recall']),
@@ -87,16 +119,17 @@ function view(state$) {
           p(['Click on the nine tiles you see to win!'])
         ]),
         div('.panel', [
-          div('.grid', state.grid.map((x) =>
+          div('.grid', grid.map((x) =>
             div('.cell'
-              + ((!state.userInputAllowed && state.puzzle.indexOf(x) !== -1) ? '.highlighted' : '')
-              + ((state.userInputAllowed && state.userSelectedCells.indexOf(x) !== -1) ? '.selected' : ''), {
+              + ((!allowed && puzzle.indexOf(x) !== -1) ? '.highlighted' : '')
+              + ((allowed && selected.indexOf(x) !== -1) ? '.selected' : ''), {
                 attrs: { 'data-index': x }
               }, [span()])
           ))
         ])
       ])
-    ]));
+    ]);
+  });
   return {
     dom: vtree$
   };
