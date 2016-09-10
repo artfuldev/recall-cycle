c54a8b663271db62e42247edb5e3d5ffcf8ff016
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-09-10 23:20:59 +0530

    Add button component

---
+++ src/components/button.ts
@@ -0,0 +1,29 @@
+import xs, { Stream } from 'xstream';
+import { DOMSource } from '@cycle/dom/xstream-typings';
+import { VNode, a } from '@cycle/dom';
+
+export interface ButtonSources {
+  classes$: Stream<string>;
+  content$: Stream<VNode[]|string>;
+  dom: DOMSource;
+}
+
+export interface ButtonSinks {
+  dom: Stream<VNode>;
+  click$: Stream<MouseEvent>;
+}
+
+function Button(sources: ButtonSources): ButtonSinks {
+  const click$ = sources.dom.select('a').events('click').map(event => event as MouseEvent);
+  const classes$ = sources.classes$;
+  const content$ = sources.content$;
+  const dom =
+    xs.combine(classes$, content$)
+      .map(([classes, content]) => a(classes, content));
+  return {
+    dom,
+    click$
+  };
+}
+
+export default Button;
